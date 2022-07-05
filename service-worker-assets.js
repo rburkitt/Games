a3b18797e4758038ce8df6d63312f9682de5d361
rburkitt <rburkitt@users.noreply.github.com>
2022-07-05 23:46:44 +0000

    Deploying to gh-pages from  @ fb341a745d775895f3b820bce9ecdffadaf68389 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -225,7 +225,7 @@
       "url": "index.html"
     },
     {
-      "hash": "sha256-B8zmRLqgr2YRVkGey84h4HvmJ2e4Mp5dMrjjtUQY4rI=",
+      "hash": "sha256-bwHtH9mq1bBx3J5jfPQed53kwbHKSvuKQWKdlZjj3hg=",
       "url": "manifest.json"
     },
     {
@@ -245,5 +245,5 @@
       "url": "words3.json"
     }
   ],
-  "version": "efukSlkp"
+  "version": "dMPEUrjT"
 };

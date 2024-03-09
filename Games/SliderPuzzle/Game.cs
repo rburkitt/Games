927218a322c4730337bb4f0363e4536c385ecaff
rburkitt <rburkitt@users.noreply.github.com>
2024-03-08 19:13:15 -0500

    ending game when solved

--- Games/SliderPuzzle/Game.cs
+++ Games/SliderPuzzle/Game.cs
@@ -38,10 +38,23 @@
             Cells[index] = emptyCell;
             Cells[emptyCellIndex] = cell;            
         }
+
+        // a function to check if the puzzle is solved
+        public bool IsSolved()
+        {
+            // if the list of cells is equal to 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,null then the puzzle is solved
+            bool retVal = Cells.Select(o => o.Value).SequenceEqual(new List<int?> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null });
+            if (retVal)
+            {
+                Cells.ForEach(x => x.Disabled = true);
+            }
+            return retVal;
+        }
     }
 
     public class Cell
     {
+        public bool Disabled { get; set; } = false;
         public int? Value { get; set; }
     }
 }

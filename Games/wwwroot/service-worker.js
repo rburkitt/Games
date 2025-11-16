f2b4e533de89de4be7daba9839169da3cc14c140
rburkitt <rburkitt@users.noreply.github.com>
2025-11-15 21:46:11 -0500

    upgrade to .net 10

--- Games/wwwroot/service-worker.js
+++ Games/wwwroot/service-worker.js
@@ -1,5 +1,5 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-// v4.0.0.4
+// v5.0.0.0
 self.addEventListener('fetch', () => { });
